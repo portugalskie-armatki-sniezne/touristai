7954446becce11ec2608c8ef3890f142a0866142
marmag0 <mikolaj.mazur05@gmail.com>
2026-03-28 15:01:23 +0100

    languages updated

--- backend/db/2_safeguards.sql
+++ backend/db/2_safeguards.sql
@@ -4,7 +4,7 @@ ALTER TABLE users
     ADD CONSTRAINT valid_username CHECK (char_length(trim(username)) >= 3);
 
 ALTER TABLE user_preferences
-    ADD CONSTRAINT valid_language CHECK (language IN ('pl', 'en', 'es', 'de'));
+    ADD CONSTRAINT valid_language CHECK (language IN ('pl', 'en'));
 
 -- migrate:down
 ALTER TABLE users
